d36da7429b3fbd49a87d82a4c1c2484a065c7247
Nametsego <Nametsegothakadu@gmail.com>
2021-10-18 05:37:53 +0200

    Full commits

---
+++ src/AntiElite/com/Main.java
@@ -0,0 +1,21 @@
+﻿package AntiElite.com;
+
+import java.io.File;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        File file = new File("C:\\Users\\Nametsego\\FileReader_TerminalApp");
+
+        if(file.exists()){
+            System.out.println("That file exists :0 !");
+            System.out.println(file.getPath());
+            System.out.println(file.getAbsolutePath());
+            System.out.println(file.isFile());
+            file.delete();
+        }else{
+            System.out.println("Path file non-existent :(");
+        }
+    }
+}
